3e289b3eb818c985c8ebbee222e7ae93bbf7666b
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2022-10-24 13:03:26 +0200

    Commit fatto a fine lezione non mi ricordo se lo sto facendo bene ahahahah

--- 61-git-remotes-merge-conflict/README.md
+++ 61-git-remotes-merge-conflict/README.md
@@ -11,9 +11,36 @@ Prima di eseguire ogni operazione sul worktree o sul repository,
 si verifichi lo stato del repository con `git status`.
 
 1. Si cloni localmente il repository
+
+❯ git clone https://github.com/APICe-at-DISI/OOP-git-merge-conflict-test.git
+Cloning into 'OOP-git-merge-conflict-test'...
+remote: Enumerating objects: 12, done.
+remote: Counting objects: 100% (4/4), done.
+remote: Compressing objects: 100% (3/3), done.
+remote: Total 12 (delta 1), reused 1 (delta 1), pack-reused 8
+Receiving objects: 100% (12/12), done.
+Resolving deltas: 100% (2/2), done.
 2. Ci si assicuri di avere localmente entrambi i branch remoti
+
+❯ git branch -a
+
+* master
+  remotes/origin/HEAD -> origin/master
+  remotes/origin/feature
+  remotes/origin/master
+
 3. Si faccia il merge di `feature` dentro `master`, ossia: si posizioni la `HEAD` su `master`
    e da qui si esegua il merge di `feature`
+
+❯ git checkout master
+Already on 'master'
+Your branch is up to date with 'origin/master'.
+
+❯ git merge origin/feature
+Auto-merging HelloWorld.java
+CONFLICT (content): Merge conflict in HelloWorld.java
+Automatic merge failed; fix conflicts and then commit the result.
+
 4. Si noti che viene generato un **merge conflict**!
 5. Si risolva il merge conflict come segue:
    - Il programma Java risultante deve stampare sia il numero di processori disponibili
@@ -22,5 +49,27 @@ si verifichi lo stato del repository con `git status`.
      (funzionalità presente su `feature`)
 6. Si crei un nuovo repository nel proprio github personale
 7. Si aggiunga il nuovo repository creato come **remote** e si elenchino i remote
+
+❯ git remote add merge-conflict git@github.com:Martinetto33/merge-conflict.git
+❯ git remote -v
+merge-conflict  git@github.com:Martinetto33/merge-conflict.git (fetch)
+merge-conflict  git@github.com:Martinetto33/merge-conflict.git (push) 
+origin  https://github.com/APICe-at-DISI/OOP-git-merge-conflict-test.git (fetch)
+origin  https://github.com/APICe-at-DISI/OOP-git-merge-conflict-test.git (push)
 8. Si faccia push del branch `master` sul proprio repository
+
+❯ git push -u merge-conflict master
+Enumerating objects: 9, done.
+Counting objects: 100% (9/9), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (6/6), done.
+Writing objects: 100% (9/9), 1012 bytes | 84.00 KiB/s, done.
+Total 9 (delta 1), reused 9 (delta 1), pack-reused 0
+remote: Resolving deltas: 100% (1/1), done.
+To github.com:Martinetto33/merge-conflict.git
+ * [new branch]      master -> master
+branch 'master' set up to track 'merge-conflict/master'.
+
 9. Si setti il branch remoto `master` del nuovo repository come *upstream* per il proprio branch `master` locale
+❯ git branch --set-upstream-to=merge-conflict/master
+branch 'master' set up to track 'merge-conflict/master'.
--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,15 +1,56 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
  *
  */
 public final class UseListsAndMaps {
+    private static final int LOWER_LIMIT = 1000;
+    private static final int UPPER_LIMIT = 2000;
+    private static final int AMOUNT = 100_000;
+    private static final int TOTAL_READS = 1000;
 
     private UseListsAndMaps() {
+    }
+
+    static void filler(List<Integer> input) {
+        long time = System.nanoTime();
+        long millis;
+        for(int i = 0; i <= UseListsAndMaps.AMOUNT; i++) {
+            input.add(i);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println ("Time elapsed for " 
+        + input.getClass() 
+        + ": " 
+        + time
+        + "ns ("
+        + millis
+        + "ms)");
+    }
+
+    static void reader (List<Integer> input) {
+        long time = System.nanoTime();
+        long millis;
+        for (int i = 0; i <= TOTAL_READS; i++) {
+            input.get(input.size() / 2);
+        }
+        time = System.nanoTime() - time;
+        millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println ("Time elapsed for " 
+        + input.getClass() 
+        + ": " 
+        + time
+        + "ns ("
+        + millis
+        + "ms)");
     }
 
     /**
@@ -21,19 +62,31 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = UseListsAndMaps.LOWER_LIMIT; i <= UseListsAndMaps.UPPER_LIMIT; i++) {
+            list.add(i);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        LinkedList<Integer> list2 = new LinkedList<>(list);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        Integer temp = list.get(list.size() - 1); //uso l'autoboxing
+        list.set(list.size() - 1, list.get(0));
+        list.set(list.get(0), temp);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (Integer elem : list) {
+            System.out.print("|" + elem);
+        }
+        System.out.println("");
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +94,16 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        UseListsAndMaps.filler(list);
+        UseListsAndMaps.filler(list2);
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example PerfTest.java.
          */
+        UseListsAndMaps.reader(list);
+        UseListsAndMaps.reader(list2);
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
